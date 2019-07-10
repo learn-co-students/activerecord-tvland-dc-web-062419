8f3f5bf9217279ec967c08df0c23667065b898ed
Luke O'Malley <Lukeomalley95@gmail.com>
2019-07-10 14:39:16 -0400

    completed lab+hamd

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(details)
+    self.network_id = Network.find_or_create_by(details).id
+    return Network.find_by(id: network_id)
+  end
 end
